68d928021f400733dd23cb03cd79360c4369c5ef
Trần Minh Hiểu <131644516+Tmh3101@users.noreply.github.com>
2024-10-25 09:25:08 +0700

    Update tranminhhieu_THBuoi2.py

--- TH02/tranminhhieu_THBuoi2.py
+++ TH02/tranminhhieu_THBuoi2.py
@@ -11,10 +11,11 @@ def cau01():
                 break
             else:
                 tmp = int(tmp / 10)
-        if(flag):
+        if (flag):
             list.append(n)
     res = ','.join(map(str, list))
     print(res)
+
 
 def cau02():
     chuoi = input('Nhap chuoi cac so cach nhau bang dau ",": ')
@@ -22,11 +23,13 @@ def cau02():
     list = [int(x) for x in list if int(x) % 2 != 0]
     print(list)
 
+
 def cau03():
     tudien = {}
     for i in range(0, 20):
         tudien.update({(i + 1): (i + 1) ** 2})
     print(tudien)
+
 
 def cau04():
     list = [12, 24, 35, 70, 88, 120, 155]
@@ -34,15 +37,18 @@ def cau04():
         list.pop(i)
     print(list)
 
+
 def cau05():
-    list1 =  [12, 3, 6, 78, 35, 55, 120]
+    list1 = [12, 3, 6, 78, 35, 55, 120]
     list2 = [12, 24, 35, 78, 88, 120, 155]
     res = [i for i in list1 if i in list2]
     print(res)
 
+
 def cau06():
     list = [(i + 1) ** 2 for i in range(0, 20)]
     print(list[5:])
+
 
 def cau07():
     list = []
@@ -59,6 +65,7 @@ def cau07():
     tuple2 = tuple(list)
     print(tuple2)
 
+
 def cau08():
     chuoi = input('Nhap vao chuoi: ')
     res = {}
@@ -69,5 +76,91 @@ def cau08():
             res[c] = 1
     print(res)
 
-cau08()
-    
+
+def cau09():
+    chuoi = input('Nhap vao mot cau: ')
+    print(chuoi[::-1])
+
+
+def cau10():
+    chuoi = ''
+    while len(chuoi) < 2:
+        chuoi = input('Nhap vao chuoi: ')
+        if len(chuoi) < 2:
+            print('Chuoi nhap vao phai co tu 2 ky tu!!!')
+    tmp1 = chuoi[:2]
+    tmp2 = chuoi[len(chuoi) - 2:]
+    res = ''.join(map(str, tmp1 + tmp2))
+    print(res)
+
+
+def cau11():
+    chuoi1 = list(input('Nhap vao chuoi 1: '))
+    chuoi2 = list(input('Nhap vao chuoi 2: '))
+
+    res = f'{chuoi1},{chuoi2}'
+    res = list(res)
+    res[:2], res[len(chuoi1) + 2:len(chuoi1) + 4] = res[len(chuoi1) + 2:len(chuoi1) + 4], res[:2]
+    res = ''.join(res)
+    print(res)
+
+
+def cau13():
+    chuoi = input('Nhap vao chuoi: ')
+    c = chuoi[0]
+    res = c + chuoi.replace(c, '@')[1:len(chuoi)]
+    print(res)
+
+
+def getsuffix(a, b):
+    res_set = set()
+    for c1 in a:
+        for c2 in b:
+            if c1.startswith(c2) and c1[len(c2):] != '':
+                res_set.add(c1[len(c2):])
+            elif c2.startswith(c1) and c2[len(c1):] != '':
+                res_set.add(c2[len(c1):])
+    return res_set
+
+
+def cau14():
+    a = ['a', 'ac', 'ab']
+    b = ['c', 'abc']
+    print(getsuffix(a, b))
+
+
+def kiemTraBangMa(a):
+    S0 = a
+    S1 = getsuffix(S0, S0)
+    S1_old = S1
+
+    while len(S1) > 0:
+        S2 = getsuffix(S0, S1)
+        Stmp = S2.intersection(S0)
+
+        if len(S2) == 0:
+            print('Day la bang ma tach duoc')
+        elif len(Stmp) != 0:
+            print('Day la bang ma khong tach duoc')
+            break
+        elif S2 == S1_old:
+            print('Day la bang ma khong tach duoc')
+            break
+        S1_old = S1
+        S1 = S2
+
+
+def cau15():
+    kiemTraBangMa(['a', 'ac', 'ab', 'c', 'abc'])
+
+
+def cau16():
+    a = ["abc", "a", "ab", "b", "bcad"]
+    b = ["a", "ad", "bbcde", "c", "deb", "ebd"]
+    c = ["a", "ad", "bbcde", "c", "deb", "ebad"]
+    print('Kiem tra bang ma a: ', a)
+    kiemTraBangMa(a)
+    print('Kiem tra bang ma b: ', b)
+    kiemTraBangMa(b)
+    print('Kiem tra bang ma c: ', c)
+    kiemTraBangMa(c)
